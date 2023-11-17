159e533d27b9ba270cbe54fbbab5573a6a960386
aebarroso <andre.e.barroso@gmail.com>
2023-11-17 02:29:00 +0000

    modified:   about.html 	modified:   adopt.html 	modified:   asset/script.js 	modified:   contact.html 	modified:   dogdetails.html 	modified:   favorites.html 	modified:   foradoption.html 	modified:   index.html 	modified:   store.html

--- asset/script.js
+++ asset/script.js
@@ -1,3 +1,4 @@
-function menu(loc) {
-    document.location.href = loc;
+function menuNav(menu) {
+    console.log(String(menu).split("#")[1]);
+    document.location.href = String(menu).split("#")[1]+".html";
 }
