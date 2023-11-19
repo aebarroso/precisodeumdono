69a92656408ff180e2d469e6b8c466f323fca58f
aebarroso <andre.e.barroso@gmail.com>
2023-11-19 16:52:48 +0000

    modified:   about.html 	modified:   assets/script.js 	modified:   css/style.css 	modified:   dogdetails.html 	modified:   index.html 	modified:   store.html

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,18 @@
 function menuNav(menu) {
-    console.log(String(menu).split("#")[1]);
-    document.location.href = String(menu).split("#")[1]+".html";
+    document.location.href = String(menu).split("#")[1] + ".html";
 }
 
 $input = $('input[type="text"]');
 
-$('.btn').on('click',function(){
-     $val = $input.val();
+$('.btn').on('click', function () {
+    $val = $input.val();
     if ($(this).hasClass('btn-minuse')) {
-     $input.val(parseInt($val)-1);
+        $input.val(parseInt($val) - 1);
+        if (parseInt($val - 1) <= 0) {
+            $input.val(0);
+        }
     } else {
-    $input.val(parseInt($val)+1);
+        $input.val(parseInt($val) + 1);
     }
 });
 
